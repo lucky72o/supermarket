bc430cc84a4f7f4d7cdb4114371318be30b616e8
slava <gurmanov.slava@gmail.com>
2015-09-14 22:13:09 +0100

    renamed main demonstration class

--- src/test/java/demonstration/CheckoutDemonstration.java
+++ src/test/java/demonstration/CheckoutDemonstration.java
@@ -20,7 +20,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Main {
+public class CheckoutDemonstration {
 
     private List<CartEntry> cartEntries = new ArrayList<>();
 
